00dbb7bb6276b1d955bbbdb55d4a5adfc00ca74e
SoniGautam222 <gautamjisoni222@gmail.com>
2023-04-14 00:42:15 +0530

    final commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/');
+mongoose.connect('mongodb://127.0.0.1:27017/todo-list');
 
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error connecting to the database'));
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express=require('express');
 const port=8000;
 const path=require('path');
 const db=require('./config/mongoose');
-const { log } = require('console');
 const app=express();
 
 app.set('view engine', 'ejs');
@@ -10,25 +9,46 @@ app.set('views',path.join(__dirname,'views'));
 
 app.use(express.urlencoded());
 app.use(express.static("assets"));
+
+const Schema=require('./models/Schema')
+
 var todoList=[];
 
-app.get('/',function(req,res){
+app.get('/',async function(req,res){
+    let lists=await Schema.find({});
      res.render('index',{
-        todoList: todoList 
+        todoList:lists 
     });
 });
 
-app.post('/',function(req,res){
-    todoList.push({
-        description:req.body.description,
-        date:req.body.date,
-        category:req.body.category,
-        
-    });
+app.post('/',async function(req,res){
+    try{
+        let todo=await Schema.create({
+            description:req.body.description,
+            date:req.body.date,
+            category:req.body.category,
+        })
+    
+    }catch(err){
+        console.log(`error found`);
+    }
     return res.redirect('/');    
 });
 
-
+app.post('/delete',async function(req,res){
+    try{
+    let checkedItemid=req.body.check;
+    await Schema.findByIdAndDelete(checkedItemid);
+    
+    res.redirect('back');
+   
+   
+    }
+    catch(err){
+        console.log("got the error");
+    }
+    
+});
 
 app.listen(port,function(err){
     if(err){
