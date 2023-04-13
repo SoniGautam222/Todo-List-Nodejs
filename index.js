acfdc5ae2ce25a767835250d38c5dcf9693f1112
SoniGautam222 <gautamjisoni222@gmail.com>
2023-04-13 17:02:45 +0530

    added the view part

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express=require('express');
 const port=8000;
+const path=require('path');
 
 const app=express();
+
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+
+app.use(express.urlencoded());
+app.use(express.static("assets"));
+
+var todoList = [
+    {
+        
+    }
+]
+
+
+app.get('/',function(req,res){
+    return res.render('index',{
+        title: 'Todo Application'
+    });
+});
 
 app.listen(port,function(err){
     if(err){
