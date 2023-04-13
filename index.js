20df3ce6f83bb011173e7e8314c9d6e8c86bdf69
SoniGautam222 <gautamjisoni222@gmail.com>
2023-04-13 22:10:28 +0530

    update the add part

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const port=8000;
 const path=require('path');
 const db=require('./config/mongoose');
+const { log } = require('console');
 const app=express();
 
 app.set('view engine', 'ejs');
@@ -9,12 +10,25 @@ app.set('views',path.join(__dirname,'views'));
 
 app.use(express.urlencoded());
 app.use(express.static("assets"));
+var todoList=[];
 
 app.get('/',function(req,res){
-    return res.render('index',{
-        title: 'Todo Application'
+     res.render('index',{
+        todoList: todoList 
     });
 });
+
+app.post('/',function(req,res){
+    todoList.push({
+        description:req.body.description,
+        date:req.body.date,
+        category:req.body.category,
+        
+    });
+    return res.redirect('/');    
+});
+
+
 
 app.listen(port,function(err){
     if(err){
